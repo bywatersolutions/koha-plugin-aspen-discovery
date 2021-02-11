3a1a13fc50a80ad1ab7bdb94a054ee96122952da
Kyle M Hall <kyle@bywatersolutions.com>
2021-02-11 11:13:52 -0500

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/AspenDiscovery.pm
@@ -0,0 +1,78 @@
+package Koha::Plugin::Com::ByWaterSolutions::ItemMessages;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use C4::Auth;
+use C4::Context;
+
+use Mojo::JSON qw(decode_json to_json);
+
+our $VERSION = "{VERSION}";
+our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
+
+our $metadata = {
+    name            => 'Aspen Discovery Plugin',
+    author          => 'Tomas Cohen Arazi',
+    date_authored   => '2021-02-11',
+    date_updated    => "1900-01-01",
+    minimum_version => $MINIMUM_VERSION,
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'A plugin for Aspen Discovery integration',
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    my $dt = dt_from_string();
+    $self->store_data( { last_upgraded => $dt->ymd('-') . ' ' . $dt->hms(':') } );
+
+    return 1;
+}
+
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+
+    my $spec_str = $self->mbf_read('openapi.json');
+    my $spec     = decode_json($spec_str);
+
+    return $spec;
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+    
+    return 'aspen_discovery';
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,6 @@
+# Aspen Discovery plugin for Koha
+A plugin to integrate Apsen Discovery with Koha
+
+## Setup
+* Download and install the plugin
+* That is all
