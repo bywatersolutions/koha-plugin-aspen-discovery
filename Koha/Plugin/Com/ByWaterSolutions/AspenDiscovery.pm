9e0b7a1f9e5537437211703699ac4527e398d910
Tomas Cohen Arazi <tomascohen@theke.io>
2021-02-22 18:06:21 -0300

    Issue #3: Rename the main plugin library
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Plugin/Com/ByWaterSolutions/AspenDiscovery.pm
+++ Koha/Plugin/Com/ByWaterSolutions/AspenDiscovery.pm
@@ -1,4 +1,19 @@
-package Koha::Plugin::Com::ByWaterSolutions::ItemMessages;
+package Koha::Plugin::Com::ByWaterSolutions::AspenDiscovery;
+
+# This file is part of koha-plugin-aspen-discovery.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 use Modern::Perl;
 
@@ -37,7 +52,6 @@ sub new {
 
     return $self;
 }
-
 
 sub install() {
     my ( $self, $args ) = @_;
